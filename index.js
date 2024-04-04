accdfa380257a6b7fa582b31d7d3c791fed34605
paulaingate <49568567+paulaingate@users.noreply.github.com>
2024-04-04 09:15:18 +0100

    website (#1)
    * website
    
    * remove comment
    
    * add title + url to carousel, page title and favicon

---
+++ index.js
@@ -0,0 +1,37 @@
+import images from "./assets/images.json" assert { type: "json" };
+
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+const carouselImg = document.getElementById("carousel-img");
+const carouselTitle = document.getElementById("carousel-title");
+
+let currentImgIndex = 0;
+const totalImages = images.length;
+
+leftArrow.addEventListener("click", () => {
+  if (currentImgIndex === 0) {
+    currentImgIndex = totalImages - 1;
+  } else {
+    currentImgIndex -= 1;
+  }
+  setCarouselImg();
+});
+
+rightArrow.addEventListener("click", () => {
+  if (currentImgIndex === totalImages - 1) {
+    currentImgIndex = 0;
+  } else {
+    currentImgIndex += 1;
+  }
+  setCarouselImg();
+});
+
+const setCarouselImg = () => {
+  const imageData = images[currentImgIndex];
+  carouselImg.style.backgroundImage = `url(/assets/${imageData.path})`;
+  carouselTitle.innerText = imageData.title;
+  carouselTitle.href = imageData.url;
+  carouselTitle.target = '_blank';
+};
+
+setCarouselImg();
